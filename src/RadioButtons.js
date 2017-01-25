18a79cebab9d4ff2b0ee9b9cfe508a5b61c944cb
wenche <wenche.tollevsen@gmail.com>
2017-01-25 15:17:45 +0100

    Custom class container for radio buttons

--- src/RadioButtons.js
+++ src/RadioButtons.js
@@ -81,7 +81,7 @@ export default class RadioButtons extends React.Component {
     return true;
   }
   render() {
-    const containerClasses = 'form__control-group form__control-group--radio' + (!this.state.isValid ? ' form__error' : '');
+    const containerClasses = 'form__control-group form__control-group--radio' + this.props.className + (!this.state.isValid ? ' form__error' : '');
     const radios = this.props.options.map((o, index) => {
       let option = o;
       if (typeof option === 'string') {
@@ -118,6 +118,7 @@ export default class RadioButtons extends React.Component {
 
 RadioButtons.propTypes = {
   name: React.PropTypes.string.isRequired,
+  className: React.PropTypes.string,
   value: React.PropTypes.string,
   errorText: React.PropTypes.string,
   store: React.PropTypes.string.isRequired,
@@ -130,4 +131,5 @@ RadioButtons.propTypes = {
 
 RadioButtons.defaultProps = {
   required: false,
+  className: '',
 };
